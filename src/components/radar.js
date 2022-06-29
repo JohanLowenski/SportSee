1a297440de2eaa464301848bab2eb27d1ea6f77c
Johan Alger <johan.alger@gmail.com>
2022-06-29 18:21:16 +0200

    Modification radar

--- src/components/radar.js
+++ src/components/radar.js
@@ -1,55 +1,85 @@
 // import PropTypes from "prop-types";
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, ResponsiveContainer } from "recharts";
 
-const Spider = (props) => {
-  const USER_PERFORMANCE = {
-    kind: {
-      1: "cardio",
-      2: "energy",
-      3: "endurance",
-      4: "strength",
-      5: "speed",
-      6: "intensity",
+const Spider = () => {
+  const data = [
+    {
+      value: 80,
+      kind: 1,
     },
-    data: [
-      {
-        value: 80,
-        kind: 1,
-      },
-      {
-        value: 120,
-        kind: 2,
-      },
-      {
-        value: 140,
-        kind: 3,
-      },
-      {
-        value: 50,
-        kind: 4,
-      },
-      {
-        value: 200,
-        kind: 5,
-      },
-      {
-        value: 90,
-        kind: 6,
-      },
-    ],
-  };
-  const renderCustomAxisTick = ({ x, y, payload }) => {
-    console.log(payload);
-    const kind = payload.kind;
+    {
+      value: 120,
+      kind: 2,
+    },
+    {
+      value: 140,
+      kind: 3,
+    },
+    {
+      value: 50,
+      kind: 4,
+    },
+    {
+      value: 200,
+      kind: 5,
+    },
+    {
+      value: 90,
+      kind: 6,
+    },
+  ];
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomAxisTick = ({ cx, cy, payload }) => {
+    const radius = 100;
+    let x = cx + radius * Math.cos(-payload.coordinate * RADIAN);
+    let y = cy + radius * Math.sin(-payload.coordinate * RADIAN);
+    /* Adding 10 to the y value of the text. */
+    let kind = "";
+    switch (payload.value) {
+      case 1:
+        kind = "Intensité";
+        y += 10;
+        break;
+      case 2:
+        kind = "Vitesse";
+        y += 10;
+        x += 10;
+        break;
+      case 3:
+        kind = "Force";
+        y -= 10;
+        x += 10;
+        break;
+      case 4:
+        kind = "Endurance";
+        y -= 10;
+        break;
+      case 5:
+        kind = "Energie";
+        y -= 10;
+        break;
+      case 6:
+        kind = "Cardio";
+        y += 10;
+        break;
+      default:
+        break;
+    }
+
     return (
-      <text x={x - 5} y={y + 15}>
+      <text x={x - 25} y={y + 5} fill="#fff" fontSize={12}>
         {kind}
       </text>
     );
   };
+  // const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  //   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  //   const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  //   const y = cy + radius * Math.sin(-midAngle * RADIAN);
   return (
     <ResponsiveContainer width="100%" height="100%">
-      <RadarChart cx="50%" cy="50%" outerRadius="60%" data={USER_PERFORMANCE.data}>
+      <RadarChart cx="50%" cy="50%" outerRadius="60%" data={data}>
         <PolarGrid polarRadius={[10, 20, 40, 60]} radialLines={false} />
         <PolarAngleAxis
           dataKey="kind"
@@ -57,9 +87,10 @@ const Spider = (props) => {
           tickLine={false}
           dy={4}
           tick={renderCustomAxisTick}
+
           // tick={{ fontSize: 12, fontWeight: 500 }}
         />
-        <Radar dataKey="A" fillOpacity={0.7} stroke="transparent" fill="#ff0101" />
+        <Radar dataKey="value" fillOpacity={0.7} stroke="transparent" fill="#ff0101" />
       </RadarChart>
     </ResponsiveContainer>
   );
--- src/components/useractivity.js
+++ src/components/useractivity.js
@@ -1,45 +1,44 @@
 // import PropTypes from "prop-types";
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
-import { USER_ACTIVITY } from "../info";
-
+// import { USER_ACTIVITY } from "../info";
 const Activities = () => {
-  // const USER_ACTIVITY = [
-  //   {
-  //     day: "2020-07-01",
-  //     kilogram: 70,
-  //     calories: 240,
-  //   },
-  //   {
-  //     day: "2020-07-02",
-  //     kilogram: 69,
-  //     calories: 220,
-  //   },
-  //   {
-  //     day: "2020-07-03",
-  //     kilogram: 70,
-  //     calories: 280,
-  //   },
-  //   {
-  //     day: "2020-07-04",
-  //     kilogram: 70,
-  //     calories: 500,
-  //   },
-  //   {
-  //     day: "2020-07-05",
-  //     kilogram: 69,
-  //     calories: 160,
-  //   },
-  //   {
-  //     day: "2020-07-06",
-  //     kilogram: 69,
-  //     calories: 162,
-  //   },
-  //   {
-  //     day: "2020-07-07",
-  //     kilogram: 69,
-  //     calories: 390,
-  //   },
-  // ];
+  const USER_ACTIVITY = [
+    {
+      day: "2020-07-01",
+      kilogram: 70,
+      calories: 240,
+    },
+    {
+      day: "2020-07-02",
+      kilogram: 69,
+      calories: 220,
+    },
+    {
+      day: "2020-07-03",
+      kilogram: 70,
+      calories: 280,
+    },
+    {
+      day: "2020-07-04",
+      kilogram: 70,
+      calories: 500,
+    },
+    {
+      day: "2020-07-05",
+      kilogram: 69,
+      calories: 160,
+    },
+    {
+      day: "2020-07-06",
+      kilogram: 69,
+      calories: 162,
+    },
+    {
+      day: "2020-07-07",
+      kilogram: 69,
+      calories: 390,
+    },
+  ];
   const renderCustomAxisTick = ({ x, y, payload }) => {
     const dayX = payload.index + 1;
     return (
@@ -55,19 +54,27 @@ const Activities = () => {
         margin={{
           top: 80,
           right: 48,
-          left: 20,
+          left: 60,
           bottom: 5,
         }}
         barGap={8}
       >
+        <text x={60} y={31} fontSize={15} fontWeight={500}>
+          Activité quotidienne
+        </text>
+        <text className="cal" x={680} y={31} fill="#9B9EAC">
+          Calories (kcal)
+        </text>
+        <text className="kilo" x={500} y={31} fill="#9B9EAC">
+          Poids (kg)
+        </text>
         <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#dedede" />
         <XAxis
           dataKey="day"
           dy={5}
           tickLine={false}
           tick={renderCustomAxisTick}
-          // tick={{ fontSize: 14, fontWeight: 500, renderCustomAxisTick }}
-          padding={{ left: -27, right: -27 }}
+          padding={{ left: -46, right: -46 }}
           stroke="#9B9EAC"
         />
         <YAxis yAxisId="kg" orientation="right" stroke="#9B9EAC" axisLine={false} tickLine={false} dx={20} />
