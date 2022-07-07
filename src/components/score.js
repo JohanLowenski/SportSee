7cd84ce330bdd06d50b94a051ef395e1b5040bf6
Johan Alger <johan.alger@gmail.com>
2022-07-07 21:44:15 +0200

    suppression de score .scss

--- src/components/score.js
+++ src/components/score.js
@@ -3,7 +3,6 @@ import { RadialBarChart, RadialBar, ResponsiveContainer, Cell } from "recharts";
 import { USER_MAIN_DATA } from "../info";
 import { getUserData } from "./service/dataApi";
 import PropTypes from "prop-types";
-import "./css/score.css";
 const Score = () => {
   const [dataScore, setData] = useState(USER_MAIN_DATA[0].todayScore * 100);
   useEffect(() => {
@@ -18,39 +17,35 @@ const Score = () => {
     { todayScore: 100, color: "transparent" },
   ];
   return (
-    <div className="scored">
-      <div className="all">
-        <div className="scoreTitle">Score</div>
-        <div className="scoreData">{dataScore}%</div>
-        <div className="ofYour">de votre</div>
-        <div className="obj">objectif</div>
-      </div>
-      <ResponsiveContainer width="100%" height="100%">
-        <RadialBarChart
-          cx={120}
-          cy={130}
-          innerRadius={70}
-          outerRadius={90}
-          startAngle={90}
-          endAngle={360 + 90}
-          data={data}
-        >
-          {/* <text x={10} y={31} fill="#000">
-            Score
-          </text> */}
-          <RadialBar dataKey="todayScore" cornerRadius="50%">
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={entry.color} cornerRadius="50%" />
-            ))}
-          </RadialBar>
-          {/* <text x="50%" y="50%">
-          {dataScore}% de votre
-          <br />
+    <ResponsiveContainer width="100%" height="100%">
+      <RadialBarChart
+        cx={120}
+        cy={150}
+        innerRadius={70}
+        outerRadius={90}
+        startAngle={90}
+        endAngle={360 + 90}
+        data={data}
+      >
+        <text x={10} y={31} fill="#000">
+          Score
+        </text>
+        <RadialBar dataKey="todayScore" cornerRadius="50%">
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={entry.color} cornerRadius="50%" />
+          ))}
+        </RadialBar>
+        <text stroke="#282D30" x={105} y={130}>
+          {dataScore}%
+        </text>
+        <text fill="#74798C" x={90} y={150}>
+          de votre
+        </text>
+        <text fill="#74798C" x={90} y={170}>
           objectif
-        </text> */}
-        </RadialBarChart>
-      </ResponsiveContainer>
-    </div>
+        </text>
+      </RadialBarChart>
+    </ResponsiveContainer>
   );
 };
 export default Score;
