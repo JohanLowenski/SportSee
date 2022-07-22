0aedd7ba97cf4df2805f2bd1ed09f9ecb0d15bde
Johan Alger <johan.alger@gmail.com>
2022-07-22 23:07:13 +0200

    test cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,12 +7,30 @@ import { getUserData } from "./service/dataApi";
 import React, { useEffect, useState } from "react";
 import "./css/alimentation.css";
 
-const Cards = (props) => {
+const Cards = () => {
   const [data, setData] = useState(null);
   useEffect(() => {
     getUserData(process.env.REACT_APP_USER_ID)
       .then((res) => {
-        setData(res.data);
+        let keyData = Object.entries(res.keyData);
+
+        keyData.forEach((element) => {
+          switch (element[0]) {
+            case "calorieCount":
+              element.push("Calories");
+              element[1] = (element[1] / 1000).toFixed(3);
+              break;
+            case "proteinCount":
+              element.push("protei");
+              break;
+            case "carbohydrateCount":
+              element.push("carbohydra");
+              break;
+            default:
+          }
+          element[0] === "calorieCount" ? element.push("kCal") : element.push("g");
+        });
+        setData(keyData);
       })
       .catch((err) => {});
   }, []);
@@ -20,34 +38,49 @@ const Cards = (props) => {
     <section className="section_alimentation">
       {data && (
         <div className="card_contain">
+          {Object.entries(data.keyData).map((item, index) => (
+            <div id="calories" className="margin">
+              <img className="section_img" src={Calories} alt="calories" />
+              <div className="section_text">
+                <p className="section_text-number">{(item[1] / 1000).toFixed(3)}kCal</p>
+                <p className="section_text-title">{item[0].slice(0, -5)}</p>
+                <p>
+                  {item[0]},{item[1]},{index},{item[0].slice(0, -5)}
+                </p>
+              </div>
+            </div>
+          ))}
+
+          {/* {data && (
           <div id="calories" className="margin">
-            <img className="section_img" src={Calories} alt="Logo" />
+            <img className="section_img" src={Calories} alt="calories" />
             <div className="section_text">
               <p className="section_text-number">{(data.keyData.calorieCount / 1000).toFixed(3)}kCal</p>
               <p className="section_text-title">Calories</p>
             </div>
           </div>
           <div id="protein" className="margin">
-            <img className="section_img" src={Protein} alt="Logo" />
+            <img className="section_img" src={Protein} alt="protein" />
             <div className="section_text">
               <p className="section_text-number">{data.keyData.proteinCount}g</p>
               <p className="section_text-title">Protéines</p>
             </div>
           </div>
           <div id="carbohydrate" className="margin">
-            <img className="section_img" src={Carbohydrate} alt="Logo" />
+            <img className="section_img" src={Carbohydrate} alt="carbohydrate" />
             <div className="section_text">
               <p className="section_text-number">{data.keyData.carbohydrateCount}g</p>
               <p className="section_text-title">Glucides</p>
             </div>
           </div>
           <div id="lipid" className="margin">
-            <img className="section_img-end" src={Lipid} alt="Logo" />
+            <img className="section_img-end" src={Lipid} alt="lipid" />
             <div className="section_text">
               <p className="section_text-number">{data.keyData.lipidCount}g</p>
               <p className="section_text-title">Lipides</p>
             </div>
           </div>
+        )} */}
         </div>
       )}
     </section>
